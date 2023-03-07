df6b6c3e178faa8708a21e196cb78a3fb7e7ee44
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-03-07 20:58:39 +0100

    first commit

---
+++ lua/tommy/set.lua
@@ -0,0 +1,36 @@
+-- general settings
+
+
+-- line numebr
+vim.opt.nu = true
+
+-- indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- command mode search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- never less than 8 lines below or on the cursor
+vim.opt.scrolloff = 8
+
+-- set leader key
+vim.g.mapleader = " "
+
+-- disable mouse by default
+vim.o.mouse = "c"
+
+-- change key to switch windows (same syntax can be used for any command)
+vim.api.nvim_set_keymap('n', '<C-q>', '<C-w><C-w>', { noremap = true })
+
+-- utomatically triggers the NvimTree when you open neovim (same syntax can be used for any command)
+--vim.cmd("autocmd VimEnter * :NvimTreeToggle")
+
+-- Remap NvimTreeToggle to T
+vim.api.nvim_set_keymap('n', '<leader>t', ':T<CR>', { noremap = true })
+-- Define T command to toggle NvimTree
+vim.cmd([[command! T :NvimTreeToggle]])
